67278922043f58cffb31434a73cb02101973c45d
Marcus L. Jensen <lomztein2@gmail.com>
2020-02-28 12:54:10 +0100

    NetBeans can go fuck itself

---
+++ NetBeansApplication/Entities/src/Installer.java
@@ -0,0 +1,21 @@
+
+import org.openide.modules.ModuleInstall;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lomztein
+ */
+public class Installer extends ModuleInstall {
+    
+    @Override
+    public void restored () {
+        System.out.println ("I am so tired of Netbeans right now please god make it stop");
+    }
+    
+}
